9c3a0afa2a273b4a656dfdfceb45ec16bd19137e
zbyr20 <zubeyiraslan828@gmail.com>
2026-08-14 17:01:25 +0300

    metodların temel yapısını öğrendim👌

---
+++ PythondaFonksiyonlar/functions.py
@@ -0,0 +1,11 @@
+def sayHello(name = 'user'):
+        return 'Hello ' + name
+msg = sayHello('Zübeyir')
+print(msg)
+def total(num1,num2):
+        return num1 + num2
+print(total(2,7))
+def yasHesapla(dogumYili):
+        return 2026 - dogumYili
+yas = yasHesapla(1999)
+print(yas)
